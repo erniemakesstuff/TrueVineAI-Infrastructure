824be6d9a3ad85dfe51b7a45b5b6fd9a45ccb436
Ernie Castro <erniemakesstuff@gmail.com>
2024-09-20 18:15:26 -0700

    Setting sns-sqs queue policies

--- main.tf
+++ main.tf
@@ -54,6 +54,58 @@ resource "aws_sns_topic_subscription" "media_to_media_text_sqs_target" {
   endpoint  = aws_sqs_queue.media_text_queue.arn
 }
 
+data "aws_iam_policy_document" "allow_sns_to_sqs_ledger" {
+  statement {
+    sid    = "First"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.ledger_queue.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_sns_topic.ledger_topic.arn]
+    }
+  }
+}
+
+resource "aws_sqs_queue_policy" "ledger_queue_policy" {
+  queue_url = aws_sqs_queue.ledger_queue.id
+  policy    = data.aws_iam_policy_document.allow_sns_to_sqs_ledger.json
+}
+
+data "aws_iam_policy_document" "allow_sns_to_sqs_media" {
+  statement {
+    sid    = "First"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.media_text_queue.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_sns_topic.media_topic.arn]
+    }
+  }
+}
+
+resource "aws_sqs_queue_policy" "media_text_queue_policy" {
+  queue_url = aws_sqs_queue.media_text_queue
+  policy    = data.aws_iam_policy_document.allow_sns_to_sqs_media.json
+}
+
 resource "aws_s3_bucket" "media_bucket" {
   bucket = "${var.s3_media_bucket_name}"
 }
