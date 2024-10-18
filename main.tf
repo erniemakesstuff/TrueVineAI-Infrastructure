352efc4911c6969d2b0b9686e25b18aad1b45ba3
Ernie Castro <erniemakesstuff@gmail.com>
2024-10-18 09:29:35 -0700

    Correcting media render queue name

--- main.tf
+++ main.tf
@@ -76,8 +76,8 @@ resource "aws_sqs_queue" "media_render_dlq_queue" {
 }
 
 resource "aws_sqs_queue" "media_render_queue" {
-  name                      = "${var.sqs_visibility_timeout_media}"
-  visibility_timeout_seconds = "${var.sqs_visibility_timeout}"
+  name                      = "${var.sqs_name_media_render}"
+  visibility_timeout_seconds = "${var.sqs_visibility_timeout_media}"
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.media_render_dlq_queue.arn
     maxReceiveCount = "${var.sqs_max_receive_count}"
