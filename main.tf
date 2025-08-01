f0baca05e7a078aab736f24b891a084c2dd674bb
valerm09 <valerm09@gmail.com>
2025-08-01 23:36:30 +0000

    bugfix removing duplicate desired size arg

--- main.tf
+++ main.tf
@@ -806,7 +806,6 @@ resource "aws_autoscaling_group" "app_asg" {
   launch_template {
     id = aws_launch_template.app_launch_template.id
   }
-  desired_capacity          = 1
   health_check_type         = "ELB"
   health_check_grace_period = 300
 
